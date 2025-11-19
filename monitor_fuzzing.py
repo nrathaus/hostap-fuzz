355298529f17afc4bf76fe3af6508563565720b0
Noam Rathaus <rathaus@gmail.com>
2025-11-19 21:56:30 +0200

    Make script a bit more verbose

--- monitor_fuzzing.py
+++ monitor_fuzzing.py
@@ -1,29 +1,37 @@
+#!/usr/bin/python3
+# You need to install scapy to get this code to work
+import sys
 import json
 import subprocess
 
 import scapy
 import scapy.layers.dot11
 
+print("Starting 'hostapd'")
 process = subprocess.Popen(
-    ["./hostapd", "hostapd-wpa3.conf"],
+    ["./hostapd/hostapd", "hostapd/hostapd-wpa3.conf"],
     stdout=subprocess.PIPE,
     stderr=subprocess.STDOUT,
     text=True,
     bufsize=1,
 )
 
+if process is None:
+    print("Failed to start process")
+    sys.exit(0)
+
 for line in process.stdout:
+    print(line, end="")
     if line.startswith("[fuzz] "):
-        print(line)
         json_str = line[len("[fuzz] ") :]
 
         json_obj = json.loads(json_str)
 
         if json_obj["msg"] == "fuzz":
             data = json_obj["data"]
-            print(f"{data=}")
+            # print(f"{data=}")
             data_unhex = bytes.fromhex(data)
-            print(f"{data_unhex=}")
+            # print(f"{data_unhex=}")
 
             packet = scapy.layers.dot11.Dot11(data_unhex)
 
